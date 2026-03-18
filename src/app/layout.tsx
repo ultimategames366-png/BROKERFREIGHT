d6dc2f7d9eb27566fd4896bb37925c5ecf577154
ultimategames366-png <ultimategames366@gmail.com>
2026-03-18 10:47:58 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
-
+<link rel="manifest" href="/manifest.json" />
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
